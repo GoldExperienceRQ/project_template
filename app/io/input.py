1aec6f818e6f5822ce2098065640b470e018833a
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2024-03-11 16:28:49 +0100

    declared basic functionality in input and output files

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def get_console_input():
+    pass
+
+
+def read_file(file_path):
+    pass
+
+
+def read_file_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def write_to_console(value):
+    pass
+
+
+def write_to_file(file_path, value):
+    pass
+
+
+def write_to_file_with_pandas(file_path, value):
+    pass
